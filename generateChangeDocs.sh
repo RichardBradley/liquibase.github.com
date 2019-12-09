b8325bffb2c1e9d65a56a81e2fffbe5d38438076
Steve Donie <sdonie@datical.com>
2019-12-09 16:15:26 -0600

    improved process for running doc generator.

---
+++ generateChangeDocs.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# re-build the java doc generator and run it. 
+
+pushd _doc_generators
+mvn clean package
+popd
+java -jar liquibase-docgenerator-1.0.0.local-SNAPSHOT.jar
+
+# show some info useful to the user
+echo ""
+echo "------- created liquibase docgenerator using the following liquibase version -------"
+grep \<liquibase.version\> _doc_generators/pom.xml
+echo ""
+

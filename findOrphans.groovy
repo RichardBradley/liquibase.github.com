9b41fdc826838cb3690f845e8c39586acc036add
Steve Donie <sdonie@datical.com>
2019-12-11 16:25:52 -0600

    additional option to show links to a particular page

--- findOrphans.groovy
+++ findOrphans.groovy
@@ -1,7 +1,19 @@
 #! /usr/bin/env groovy
 
-// Search through this directory recursively, looking at each .md file and then seeing if there are any references to that file
-// in any of the other pages.
+// If run with no arguments, search through this directory recursively, looking at each .md file and 
+// then seeing if there are any references to that file in any of the other pages. Output shows a
+// count of links to each page, sorted from largest to smallest.
+//
+// using the the generated _site directory, use grep to find files that mention the name of
+// each page followed by ".html". We will just update the count of 'links' in the allPages
+// map with the number of pages that mention the page in question. Then at the end, any page
+// that has zero links to it is (potentially) an orphan. There may be some that are intentionally
+// orphans. 
+// Note that if there are two pages with the same name but in different directories, we will mis-count
+// those unless we make this way smarter about looking at page names and links. 
+//
+// If run with a single argument, consider that argument as a name of a page and show the internal 
+// links to that page.
 
 @Grapes(
     @Grab(group='commons-io', module='commons-io', version='2.6')
@@ -16,8 +28,8 @@ def extensions = [:]
 
 // a wrapper closure around executing a string                                  
 // can take either a string or a list of strings (for arguments with spaces)    
-// prints all output, complains and halts on error                              
-def runCommand = { strList, dirName ->
+// returns a count of lines in the output.
+def getCountFromCommand = { strList, dirName ->
   def proc = strList.execute(null,new File(dirName))
   def stdout = new StringBuilder(), stderr = new StringBuilder()
   proc.consumeProcessOutput(stdout, stderr)
@@ -31,6 +43,17 @@ def runCommand = { strList, dirName ->
   return count
 }
 
+// a wrapper closure around executing a string                                  
+// can take either a string or a list of strings (for arguments with spaces)    
+// prints the output
+def runCommand = { strList, dirName ->
+  def proc = strList.execute(null,new File(dirName))
+  def stdout = new StringBuilder(), stderr = new StringBuilder()
+  proc.consumeProcessOutput(stdout, stderr)
+  proc.waitForOrKill(1000)
+
+  println stdout.toString()
+}
 
 //--------- main execution -----------------------------------------------------
 
@@ -79,26 +102,24 @@ currentDir.eachFileRecurse {
 }
 
 println "Total pages: ${allPages.size()}"
-print "Finding orphans "
-allPages.each { entry ->
-  count = runCommand ("grep -nHIrF -- ${entry.key}.html".toString(),"_site")
-  allPages[entry.key] = count
-  print '.'
-}
-println ''
+if (args.length == 0) {
+  print "Finding orphans "
+  allPages.each { entry ->
+    count = getCountFromCommand ("grep -nHIrF -- ${entry.key}.html".toString(),"_site")
+    allPages[entry.key] = count
+    print '.'
+  }
+  println ''
 
-def sortedPages = allPages.sort { -it.value }
-println "Pages"
-println "\t${'PageName'.padRight(80)}\tLink Count"
-sortedPages.each { entry ->
-  println "\t${entry.key.padRight(80)}\t${entry.value}"
+  def sortedPages = allPages.sort { -it.value }
+  println "Pages"
+  println "\t${'PageName'.padRight(80)}\tLink Count"
+  sortedPages.each { entry ->
+    println "\t${entry.key.padRight(80)}\t${entry.value}"
+  }
+} else {
+  println "Finding links to page named '${args[0]}.html'"
+  runCommand ("grep -nHIrF -- ${args[0]}.html".toString(),"_site")
 }
 
-// using the the generated _site directory, use grep to find files that mention the name of
-// each page followed by ".html". We will just update the count of 'links' in the allPages
-// map with the number of pages that metion the page in question. Then at the end, any page
-// that has zero links to it is (potentially) an orphan. There may be some that are intentionally
-// orphans. 
-// Note that if there are two pages with the same name but in different directories, we will mis-count
-// those unless we make this way smarter about looking at page names and links. 
 
